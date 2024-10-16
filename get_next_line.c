a5de0fdea13cbb490c6228dc2bc1b6217bd54db1
jonas <jonas.voisard@gmail.com>
2024-10-16 02:53:34 +0200

    Mhhh its better but nop

--- get_next_line.c
+++ get_next_line.c
@@ -6,81 +6,125 @@
 /*   By: jvoisard <jonas.voisard@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/12 19:32:03 by jvoisard          #+#    #+#             */
-/*   Updated: 2024/10/13 23:18:14 by jvoisard         ###   ########.fr       */
+/*   Updated: 2024/10/15 22:34:26 by jvoisard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*read_file(int fd);
-typedef struct s_content
+static char	*get_reader_nl(t_reader *reader)
 {
-	char	*str;
 	char	*cursor;
-	int		fd;
-}	t_content;
+	ssize_t	len;
 
-static void *free_content(t_content **content)
-{
-	if (!*content)
-		return (0);
-	if ((*content)->str)
-		free((*content)->str);
-	free(*content);
-	*content = NULL;
+	len = reader->len;
+	cursor = reader->cursor;
+	while (len--)
+	{
+		if (*cursor == '\n')
+			return (cursor);
+		cursor++;
+	}
 	return (0);
 }
 
-static t_content	*new_content(int fd)
+static t_reader	*next_reader(t_reader *reader, int fd)
 {
-	t_content	*content;
-
-	content = malloc(sizeof(*content));
-	if (!content)
+	if (!reader)
+	{
+		reader = malloc(sizeof(*reader));
+		if (!reader)
+			return (0);
+		reader->cursor = reader->buffer;
+		reader->len = read(fd, reader->buffer, BUFFER_SIZE);
+		if (reader->len == -1)
+		{
+			free(reader);
+			return (0);
+		}
+		reader->next = NULL;
+		reader->nl = get_reader_nl(reader);
+		reader->is_end = reader->len < BUFFER_SIZE;
+	}
+	if (reader->nl || reader->is_end)
+		return (reader);
+	reader->next = next_reader(reader->next, fd);
+	if (!reader->next)
+	{
+		free(reader);
 		return (0);
-	content->fd = fd;
-	content->str = read_file(fd);
-	content->cursor = content->str;
-	if (!content->str)
-		return (free_content(&content));
-	return (content);
+	}
+	return (reader);
 }
 
-static size_t	get_next_line_len(t_content *content)
+static ssize_t	get_line_len(t_reader *reader)
 {
-	size_t	len;
+	if (reader->is_end)
+		return (0);
+	if (reader->nl)
+		return (reader->nl - reader->cursor + 1);
+	return (reader->len + get_line_len(reader->next));
+}
 
-	len = 0;
-	while (content->cursor[len])
+static t_reader	*write_line(t_reader *reader, char *line)
+{
+	t_reader	*next;
+
+	while (reader->len && (!reader->nl || reader->cursor <= reader->nl))
 	{
-		if (content->cursor[len] == '\n')
-			return (len + 1);
-		len++;
+		*(line++) = *(reader->cursor++);
+		reader->len--;
 	}
-	return (len);
-	
+	reader->nl = get_reader_nl(reader);
+	if (reader->len)
+		return (reader);
+	if (!reader->next)
+		return (reader);
+	next = write_line(reader->next, line);
+	free(reader);
+	return (next);
+}
+
+static short	is_end(t_reader *reader)
+{
+	if (reader->len)
+		return (0);
+	if (reader->next)
+		return (is_end(reader->next));
+	return (reader->is_end);
+}
+
+static void	*free_reader(t_reader *reader)
+{
+	if (reader->next)
+		free_reader(reader->next);
+	free(reader);
+	return (0);
 }
 
 char	*get_next_line(int fd)
 {
-	static t_content	*content;
-	size_t				line_len;
-	char				*line;
-	char				*_line;
+	static t_reader	*reader;
+	ssize_t			line_len;
+	char			*line;
 
-	if (!content)
-		content = new_content(fd);
-	if (!content)
+	reader = next_reader(reader, fd);
+	if (!reader)
 		return (0);
-	line_len = get_next_line_len(content);
-	if (!line_len)
-		return (free_content(&content));
+	line_len = get_line_len(reader);
+	if (!line_len && is_end(reader))
+	{
+		free_reader(reader);
+		reader = NULL;
+		return (0);
+	}
 	line = malloc(line_len + 1);
 	if (!line)
-		return (free_content(&content));
-	_line = line;
-	while (line_len--)
-		*(_line++) = *(content->cursor++);
-	*_line = '\0';
+	{
+		//clean reader
+		return (0);
+	}
+	reader = write_line(reader, line);
+	line[line_len] = '\0';
 	return (line);
 }
